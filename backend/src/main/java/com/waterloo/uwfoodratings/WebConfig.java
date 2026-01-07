2ee70fe6873bcbde0d5eaa81b5a4a6becbdc6486
Jason Yu <tojasonyuca@gmail.com>
2026-01-06 23:06:08 -0500

    Update CORS to allow production frontend

--- backend/src/main/java/com/waterloo/uwfoodratings/WebConfig.java
+++ backend/src/main/java/com/waterloo/uwfoodratings/WebConfig.java
@@ -14,9 +14,12 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "https://uw-food-ratings-frontend.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }

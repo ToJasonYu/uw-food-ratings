e1ec61340f12e38f2790bcbd2647fe43af28634b
ToJasonYu <tojasonyuca@gmail.com>
2026-08-24 15:19:01 -0400

    hash passwords with bcrypt instead of storing/comparing plaintext

--- backend/src/main/java/com/waterloo/uwfoodratings/AuthController.java
+++ backend/src/main/java/com/waterloo/uwfoodratings/AuthController.java
@@ -2,6 +2,7 @@ package com.waterloo.uwfoodratings;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.server.ResponseStatusException;
 import java.util.Optional;
 
@@ -10,9 +11,11 @@ import java.util.Optional;
 public class AuthController {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthController(UserRepository userRepository) {
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
@@ -20,6 +23,7 @@ public class AuthController {
         if (userRepository.findByUsername(newUser.getUsername()).isPresent()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already taken");
         }
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
         return userRepository.save(newUser);
     }
 
@@ -27,7 +31,7 @@ public class AuthController {
     public User login(@RequestBody User loginRequest) {
         Optional<User> user = userRepository.findByUsername(loginRequest.getUsername());
 
-        if (user.isPresent() && user.get().getPassword().equals(loginRequest.getPassword())) {
+        if (user.isPresent() && passwordEncoder.matches(loginRequest.getPassword(), user.get().getPassword())) {
             return user.get();
         } else {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
